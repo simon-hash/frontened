aa37fd2f255b39c04676108275c52cae10d2ece9
simon <smatusch@stud.hs-heilbronn.de>
2022-07-10 17:43:46 +0200

    Get Json Data works for images and p tags

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -8,53 +8,20 @@ import Product from "../components/Product";
 
 
 function ProductScreen() {
-    const c = useParams()
-    //const product = products.getElementById((p) => p._id === c)
+  // param to match to post
+  const match = useParams();
+  const product = products.find((p) => p._id == match.id);
 
-    /*function getObjectByID(c) {
-        return products.filter(function(products){
-                return products.id === c }
-        );
-    }
-    var product = getObjectByID(c);*/
+  return <div>
 
-    /*let wantedData = products.filter(function(i) {
-    return i._id === c
-    });
-    console.log(wantedData)*/
-
-    /*let data = products.filter((a)=>{if(a._id == c){return a}});
-    console.log(data);*/
-
-    let filteredPeople = products.filter(function (currentElement) {
-      // the current value is an object, so you can check on its properties
-      return currentElement._id === c;
-    });
-
-
-    return(
-        <div>
-            <h2>Product Screen</h2>
             <Link to='/' className='btn btn-light my-3'>Go Back</Link>
 
-                <Row>
+            <Col md={6}>
+                <p>{product.name}</p>
+                <Image src={product.image} alt={product.name} fluid/>
+            </Col>
 
-                   <Col>
-
-                       <Image src={filteredPeople.image} fluid></Image>
-
-                   </Col>
-
-
-                </Row>
-
-                {/*<Row>
-                    <Col md={6}>
-                        <Image src={product.image} alt={product.name} fluid />
-                    </Col>
-                    <Col md={3}></Col>
-                </Row>*/}
-        </div>
-    )
+         </div>;
 }
+
 export default ProductScreen
